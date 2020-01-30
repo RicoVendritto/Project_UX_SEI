f3d23fdfbb0176f6081006607d19db97c37748e0
Richard Braamburg <richard.braamburg@gmail.com>
2020-01-30 16:12:35 -0500

    Update Store Info

--- src/services/api_helper.js
+++ src/services/api_helper.js
@@ -2,73 +2,73 @@ const ApiCall = props => {
   const shopArray = [
     {
       product_image: "https://i.ibb.co/kJpzZ15/product-1.jpg",
-      product_name: "Playstation 5",
-      product_price: 399.0
+      product_name: "BISTRO Baseball Jersey",
+      product_price: 25.19
     },
     {
       product_image: "https://i.ibb.co/WKC9Mxq/product-2.jpg",
-      product_name: "Playstation 5",
-      product_price: 399.0
+      product_name: "BISTRO Hoodie (dark)",
+      product_price: 43.69
     },
     {
       product_image: "https://i.ibb.co/dP71WXQ/product-3.jpg",
-      product_name: "Playstation 5",
-      product_price: 399.0
+      product_name: "BISTRO Long Sleeve T-Shirt",
+      product_price: 27.69
     },
     {
       product_image: "https://i.ibb.co/4JMDtL0/product-4.jpg",
-      product_name: "Playstation 5",
-      product_price: 399.0
+      product_name: "Bistro Pint Glass",
+      product_price: 15.89
     },
     {
       product_image: "https://i.ibb.co/GnG06MB/product-5.jpg",
-      product_name: "Playstation 5",
-      product_price: 399.0
+      product_name: "Bistro Dark T-Shirt",
+      product_price: 25.69
     },
     {
       product_image: "https://i.ibb.co/bg3MNg2/product-6.jpg",
-      product_name: "Playstation 5",
-      product_price: 399.0
+      product_name: "Bistro Mugs",
+      product_price: 14.99
     },
     {
       product_image: "https://i.ibb.co/MGywm0p/product-7.png",
-      product_name: "Playstation 5",
-      product_price: 399.0
+      product_name: "Black Cap",
+      product_price: 20.00
     },
     {
       product_image: "https://i.ibb.co/gWVQZy5/product-8.jpg",
-      product_name: "Playstation 5",
-      product_price: 399.0
+      product_name: "Dog T-Shirt",
+      product_price: 19.99
     },
     {
       product_image: "https://i.ibb.co/8dQbs5z/product-9.jpg",
-      product_name: "Playstation 5",
-      product_price: 399.0
+      product_name: "Jr. Spaghetti Tank",
+      product_price: 21.99
     },
     {
       product_image: "https://i.ibb.co/4fn8H0S/product-10.png",
-      product_name: "Playstation 5",
-      product_price: 399.0
+      product_name: "Cap",
+      product_price: 19.99
     },
     {
       product_image: "https://i.ibb.co/yQY4n1s/product-11.jpg",
-      product_name: "Playstation 5",
-      product_price: 399.0
+      product_name: "Teddy Bear",
+      product_price: 17.99
     },
     {
       product_image: "https://i.ibb.co/tsZ2q1N/product-12.png",
-      product_name: "Playstation 5",
-      product_price: 399.0
+      product_name: "Trucker Hat",
+      product_price: 14.99
     },
     {
       product_image: "https://i.ibb.co/mHq8rgG/product-13.jpg",
-      product_name: "Playstation 5",
-      product_price: 399.0
+      product_name: "Women's V-Neck Dark T-Shirt",
+      product_price: 25.99
     },
     {
       product_image: "https://i.ibb.co/XLvrFcn/product-14.jpg",
-      product_name: "Playstation 5",
-      product_price: 399.0
+      product_name: "Bistro Shirt",
+      product_price: 23.99
     }
   ];
   return shopArray;
