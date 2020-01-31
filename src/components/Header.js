03a37797d2a091c36fc02d07a1511af450957a06
Richard braamburg <richard.braamburg@gmail.com>
2020-01-30 22:28:21 -0500

    New Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,14 +2,16 @@ import React from "react";
 
 // Custom Components
 import Slider from "./SliderHeader";
-import LogoInteractive from "./LogoInteractive";
+// import LogoInteractive from "./LogoInteractive";
 import Navigation from "./Navigation";
+import LogoMonica from "./LogoMonica";
 
 const Header = () => {
   return (
     <header className="header">
       <h4 className="cash_only">CASH ONLY</h4>
-      <LogoInteractive />
+      {/* <LogoInteractive /> */}
+      <LogoMonica />
       <Navigation />
       <Slider />
     </header>
