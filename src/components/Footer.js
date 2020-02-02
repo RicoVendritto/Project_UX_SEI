ad809f870e345102037da03d40bc85c254f4183b
Richard Braamburg <richard.braamburg@gmail.com>
2020-02-02 12:47:59 -0500

    Footer visuals'

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,36 +1,32 @@
 import React from "react";
-import social from "../resources/icons/all_in_one.png";
-import grub from "../resources/icons/GrubHub.png";
+import doordash from "../resources/footer/doordash_logo.png";
+import fbigtw from "../resources/footer/fb_ig_tw.png";
+import grubhub_logo from "../resources/footer/grubhub_logo.png";
+import seemless from "../resources/footer/seemless_logo.png";
+import tny from "../resources/footer/tny_logo.png";
+import yelp from "../resources/footer/yelp_logo.png";
+import timeout from "../resources/footer/tonyc_logo.png";
 
 const Footer = () => {
   return (
     <footer className="footer">
       <div className="footer_items">
-        <div className="order_thru">
-          <h4> Order Thru</h4>
-          <img
-                className="grub"
-                src={grub}
-                alt="grub-hub"
-              />
-          <p> Door Dash </p>
-          <p> Seamless </p>
+        <div className="footer_menu order_thru">
+          <h4>Order Thru</h4>
+          <img className="footer_logo_left grub_footer" src={grubhub_logo} alt="grub-hub" />
+          <img className="footer_logo_left doordash_footer" src={doordash} alt="doordash" />
+          <img className="footer_logo_left seamless_footer" src={seemless} alt="seemless" />
         </div>
-        <div className="connect">
-          <h4> Connect With Us! </h4>
-          <p> Social Media Links </p>
-          <img
-                className="social-media"
-                src={social}
-                alt="social-media"
-              />
-          <p> Subscribe </p>
+        <div className="footer_menu connect">
+          <h4>Connect With Us!</h4>
+          <img className="social-media" src={fbigtw} alt="social-media" />
+          <button className="footer_button" onClick={e => alert("Thank you")}>SUBSCRIBE</button>
         </div>
-        <div className="press">
-          <h4> Press </h4>
-          <p> Yelp </p>
-          <p> The New Yorker</p>
-          <p> Timeout </p>
+        <div className="footer_menu press">
+          <h4>Press</h4>
+          <img className="footer_logo_right yelp_footer" src={yelp} alt="yelp" />
+          <img className="footer_logo_right tny_footer" src={tny} alt="tny" />
+          <img className="footer_logo_right tonyc_footer" src={timeout} alt="timeout" />
         </div>
       </div>
     </footer>
